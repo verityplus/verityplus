9bb7b1521674bf241a7c0f9fc6a663a6a93e654f
Yovieee <inimemangyonas@gmail.com>
2026-04-19 14:28:14 +0700

    perbaiki github pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/verityplus/' : '/',
   plugins: [
     vue(),
     vueJsx(),
