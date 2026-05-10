427891ec6e9fe32c18bee2549a4938b2990d4a82
Yovieee <inimemangyonas@gmail.com>
2026-05-10 22:36:34 +0700

    perbaiki

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -46,7 +46,10 @@ export function formatDate(
           relativeString = rtf.format(-diffInDays, 'day')
         }
 
-        return relativeString.charAt(1).toUpperCase() + relativeString.slice(2)
+        relativeString = relativeString.slice(0)
+        relativeString = relativeString.slice(-1)
+
+        return relativeString.charAt(0).toUpperCase() + relativeString.slice(1)
       }
     }
 
